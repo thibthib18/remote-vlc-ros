eabd76a868805e1f3892ee7b0c8f60f9478f6d03
thib <thibault@seervision.com>
2023-02-03 09:31:31 +0400

    format

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,9 +41,9 @@ selkies-gstreamer-resize "${SIZEW}x${SIZEH}"
 
 # Run the x11vnc + noVNC fallback web interface if enabled
 if [ "${NOVNC_ENABLE,,}" = "true" ]; then
-  if [ -n "$NOVNC_VIEWPASS" ]; then export NOVNC_VIEWONLY="-viewpasswd ${NOVNC_VIEWPASS}"; else unset NOVNC_VIEWONLY; fi
-  x11vnc -display "${DISPLAY}" -passwd "${BASIC_AUTH_PASSWORD:-$PASSWD}" -shared -forever -repeat -xkb -snapfb -threads -xrandr "resize" -rfbport 5900 ${NOVNC_VIEWONLY} &
-  /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 8080 --heartbeat 10 &
+    if [ -n "$NOVNC_VIEWPASS" ]; then export NOVNC_VIEWONLY="-viewpasswd ${NOVNC_VIEWPASS}"; else unset NOVNC_VIEWONLY; fi
+    x11vnc -display "${DISPLAY}" -passwd "${BASIC_AUTH_PASSWORD:-$PASSWD}" -shared -forever -repeat -xkb -snapfb -threads -xrandr "resize" -rfbport 5900 ${NOVNC_VIEWONLY} &
+    /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 8080 --heartbeat 10 &
 fi
 
 
@@ -52,11 +52,11 @@ KDE_START="matchbox-window-manager -use_titlebar no -use_cursor yes"
 
 # Use VirtualGL to run the KDE desktop environment with OpenGL if the GPU is available, otherwise use OpenGL with llvmpipe
 if [ -n "$(nvidia-smi --query-gpu=uuid --format=csv | sed -n 2p)" ]; then
-  export VGL_DISPLAY="${VGL_DISPLAY:-egl}"
-  export VGL_REFRESHRATE="$REFRESH"
-  vglrun +wm $KDE_START &
+    export VGL_DISPLAY="${VGL_DISPLAY:-egl}"
+    export VGL_REFRESHRATE="$REFRESH"
+    vglrun +wm $KDE_START &
 else
-  $KDE_START &
+    $KDE_START &
 fi
 
 
